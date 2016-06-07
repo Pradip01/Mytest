add8874728950529eefed36a184a9f9f675d1301
Pradip_Kolhe <pradip.kolhe@raweng.com>
2016-06-07 18:58:58 +0530

    templete edited

--- first-website/config/all.js
+++ first-website/config/all.js
@@ -7,7 +7,11 @@ module.exports = exports = {
             "relative_url_prefix": "/"
         }
     ],
-    plugins: {},
+    plugins: {
+        "myplugin" :{
+
+        }
+    },
     contentstack: {
         api_key: "bltc4d67b208873c89d",
         access_token: "bltbd17c4c5f7bcd97cb22b8c9a"
